db3d4116eb9eb98d2b7557940e440427fa143516
Gabriel Esteban <gabriel.esteban.gullon@gmail.com>
2017-04-23 10:35:38 +0200

    Set nn to global

--- connector.py
+++ connector.py
@@ -8,6 +8,7 @@ ENTRY_TABLES = ['TypeScript', 'Java', 'ApacheConf', 'JavaScript', 'Makefile',
 'C++', 'TeX', 'Objective-C', 'R', 'CSS', 'C#', 'ActionScript', 'Go', 'PHP']
 
 def start():
+    global nn
     nn = NN([5, 30, 2])
     t = nn_gen.DataGen()
     data = t.gen(3000)
@@ -21,12 +22,14 @@ def transform_langs(langs):
     return res
 
 def train(langs, answer):
+    global nn
     if nn is not None:
         nn.correct(transform_langs(langs), answer)
     else:
         raise Exception('NN hasn\'t been started.')
 
 def test(langs):
+    global nn
     if nn is not None:
         return nn.getAnswer(transform_langs(langs))
     else:
